2b19a724315c23554ade8148befbe595de7bc00e
vishalchaudhary15667 <your-vishalchaudhary@fanaticcoders.com>
2023-07-26 10:13:16 +0530

    new page add

---
+++ js/thumbnail.js
@@ -0,0 +1,11 @@
+new Drift(document.querySelector('.drift-demo-trigger'), {
+  paneContainer: document.querySelector('.item1'),
+  inlinePane: 769,
+  inlineOffsetY: -85,
+  containInline: true,
+  hoverBoundingBox: true
+});
+
+
+
+/**************slider***************** */
